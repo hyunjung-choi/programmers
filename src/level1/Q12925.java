f1eb15c1a7f14db25310039130038b53380e4a60
hyunjung-choi <hjchoi.dev@gmail.com>
2021-07-14 10:02:59 +0900

    Add level1/문자열을 정수로 바꾸기

---
+++ src/level1/Q12925.java
@@ -0,0 +1,10 @@
+package level1;
+
+// 문자열을 정수로 바꾸기
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+public class Q12925 {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
