27e568adca0c098061939f6869c735283e49b55a
최현정 <hjchoi.dev@gmail.com>
2023-11-20 23:25:38 +0900

    Add level0/a와 b 출력하기

---
+++ src/level0/Q181951.kt
@@ -0,0 +1,12 @@
+package level0
+
+/**
+ * a와 b 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181951
+ */
+
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    println("a = $a")
+    println("b = $b")
+}
