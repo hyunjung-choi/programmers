a5dbc4fcc83051d5bbb7d58834b3585810af950c
최현정 <hjchoi.dev@gmail.com>
2023-11-26 23:39:22 +0900

    Add level0/특수문자 출력하기

---
+++ src/level0/Q181948.kt
@@ -0,0 +1,10 @@
+package level0
+
+/**
+ * 특수문자 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ */
+
+fun main(args: Array<String>) {
+    println("""!@#$%^&*(\'"<>?:;""")
+}
