5f306eb300c088e8255464e1831ddb2fafaf698d
최현정 <hjchoi.dev@gmail.com>
2023-11-27 23:21:30 +0900

    Add level0/덧셈식 출력하기

---
+++ src/level0/Q181947.kt
@@ -0,0 +1,11 @@
+package level0
+
+/**
+ * 덧셈식 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181947
+ */
+
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    println("$a + $b = ${a + b}")
+}
